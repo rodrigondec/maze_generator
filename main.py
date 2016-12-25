00ab627fe2dc397736bc3e0fb55cc0402ee4e4f5
Rodrigo Castro <rodrigondec@gmail.com>
2016-12-25 17:51:45 -0300

    main adicionado

---
+++ main.py
@@ -0,0 +1,5 @@
+import string
+
+# string = input("Digite a quantidade de celulas no formato LINHAxCOLUNA (4x4): ")
+# linhas = int(string.lower().split("x")[0])
+# colunas = int(string.lower().split("x")[1])
