52d47725648d1c08377c3f0a387a57ee5a4865eb
Rodrigo Castro <rodrigondec@gmail.com>
2016-12-25 20:11:56 -0300

    gerador funcionando, precisa melhorar método de escolha randômica

--- generator.py
+++ generator.py
@@ -1,3 +1,7 @@
+from maze import Maze
+from index import Index
+from random import randint, choice
+
 class Generator(object):
 	direita = 0
 	baixo = 1
@@ -5,8 +9,41 @@ class Generator(object):
 	cima = 3
 
 	def gerar(linhas, colunas):
-		while False:
-			pass
+		maze = Maze(linhas, colunas)
 		
-	def __init__(self, linhas, colunas):
+		while not(maze.connected()):
+			index1 = Index(randint(0, linhas-1), randint(0, colunas-1))
+			index2 = Index(index1.linha, index1.coluna)
+			
+			validos = [Generator.direita, Generator.baixo, Generator.esquerda, Generator.cima]
+
+			if index1.linha == 0:
+				validos.remove(Generator.cima)
+			elif index1.linha == linhas-1:
+				validos.remove(Generator.baixo)
+
+			if index1.coluna == 0:
+				validos.remove(Generator.esquerda)
+			elif index1.coluna == colunas-1:
+				validos.remove(Generator.direita)
+
+			direcao = choice(validos)
+			if direcao == Generator.direita:
+				index2.coluna += 1
+			elif direcao == Generator.baixo:
+				index2.linha += 1
+			elif direcao == Generator.esquerda:
+				index2.coluna -= 1
+			elif direcao == Generator.cima:
+				index2.linha -= 1
+
+			print(index1)
+			print(index2)
+			maze.conectar(index1, index2)
+
+		print(maze)
+		
+	def __init__(self):
 		super(Generator, self).__init__()	
+
+Generator.gerar(12, 12)
