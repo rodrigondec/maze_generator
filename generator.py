a2d6a3f657b8829ea49aa72b893b5e2da798f658
Rodrigo Castro <rodrigondec@gmail.com>
2016-12-25 17:51:38 -0300

    generator adicionado

---
+++ generator.py
@@ -0,0 +1,12 @@
+class Generator(object):
+	direita = 0
+	baixo = 1
+	esquerda = 2
+	cima = 3
+
+	def gerar(linhas, colunas):
+		while False:
+			pass
+		
+	def __init__(self, linhas, colunas):
+		super(Generator, self).__init__()	
