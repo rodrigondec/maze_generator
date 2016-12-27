2bf813439a0fccb6222ec33925bf059ec45e3dd4
RodrigoNdeC <rodrigondec@gmail.com>
2016-12-27 14:46:52 -0300

    método connected refatorado

--- maze.py
+++ maze.py
@@ -7,7 +7,7 @@ class Maze(object):
 		super(Maze, self).__init__()
 		self.linhas = linhas
 		self.colunas = colunas
-		self.mapa = [[Cell() for x in range(colunas)] for y in range(linhas)]
+		self.mapa = [[Cell(Index(y, x)) for x in range(colunas)] for y in range(linhas)]
 		
 	def connected(self):
 		roots = []
@@ -15,9 +15,9 @@ class Maze(object):
 			for cell in self.mapa[i]:
 				roots.append(find(cell))
 
-		for root_1 in roots:
-			for root_2 in roots:
-				if root_1 != root_2:
+		for i in range(0, len(roots)):
+			for j in range(i+1, len(roots)):
+				if roots[i] != roots[j]:
 					return False
 		return True
 
@@ -28,19 +28,19 @@ class Maze(object):
 		union(celula_1, celula_2)
 
 		if index_1.linha < index_2.linha:
-			# celula_1 está em cima
+			# celula_1 esta em cima
 			self.mapa[index_1.linha][index_1.coluna].abrir_bot()
 			self.mapa[index_2.linha][index_2.coluna].abrir_top()
 		elif index_1.linha > index_2.linha:
-			# celula_1 está em baixo
+			# celula_1 esta em baixo
 			self.mapa[index_1.linha][index_1.coluna].abrir_top()
 			self.mapa[index_2.linha][index_2.coluna].abrir_bot()
 		elif index_1.coluna < index_2.coluna:
-			# celula_1 está a esquerda
+			# celula_1 esta a esquerda
 			self.mapa[index_1.linha][index_1.coluna].abrir_direita()
 			self.mapa[index_2.linha][index_2.coluna].abrir_esquerda()
 		elif index_1.coluna > index_2.coluna:
-			# celula_1 está a direita
+			# celula_1 esta a direita
 			self.mapa[index_1.linha][index_1.coluna].abrir_esquerda()
 			self.mapa[index_2.linha][index_2.coluna].abrir_direita()
 
@@ -79,5 +79,6 @@ class Maze(object):
 
 # for i in range(1, 12):
 # 	maze.conectar(Index(i-1, 0), Index(i, 0))
-
+# maze.print_c()
 # print(maze)
+# print(maze.connected())
